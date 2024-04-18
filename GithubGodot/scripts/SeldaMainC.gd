b3d73f8a250dea53190dbf78378a595ee77c5c9f
gabrll05 <142859449+gabrll05@users.noreply.github.com>
2024-04-18 01:48:57 -0600

    commit_henao

---
+++ GithubGodot/scripts/SeldaMainC.gd
@@ -0,0 +1,69 @@
+extends CharacterBody2D
+
+const speed = 100
+var current_dir = "none"
+
+func _ready():
+	$AnimatedSprite2D.play("front_idle")
+
+func _physics_process(delta):
+	player_movement(delta)
+	
+func player_movement(delta):
+	
+	if Input.is_action_pressed("move_right"):
+		current_dir = "right"
+		player_anim(1)
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("move_left"):
+		current_dir = "left"
+		player_anim(1)
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("move_up"):
+		current_dir = "up"
+		player_anim(1)
+		velocity.x = 0
+		velocity.y = -speed
+	elif Input.is_action_pressed("move_down"):
+		current_dir = "down"
+		player_anim(1)
+		velocity.x = 0
+		velocity.y = speed
+	else:
+		player_anim(0)
+		velocity.x = 0
+		velocity.y = 0
+		
+		
+	move_and_slide()
+	
+func player_anim(movement):
+	var dir = current_dir
+	var anim = $AnimatedSprite2D
+	
+	if dir == "right":
+		anim.flip_h = false
+		if movement == 1:
+			anim.play("right_side_walk")
+		elif movement == 0:
+			anim.play("right_idle")
+	elif dir == "left":
+		anim.flip_h = false 
+		if movement == 1:
+			anim.play("left_side_walk")
+		elif movement == 0:
+			anim.play("left_idle")
+	elif dir == "up":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("back_walk")
+		elif movement == 0:
+			anim.play("back_idle")
+	elif dir == "down":
+		anim.flip_h = true
+		if movement == 1:
+			anim.play("front_walk")
+		elif movement == 0:
+			anim.play("front_idle")
