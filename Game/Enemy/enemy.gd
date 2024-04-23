66e0825959dfdb9ca51146f398f676d1b367f861
Jimmy Feng Feng <jimmy.feng04@gmail.com>
2024-04-23 17:18:01 -0600

    gdextension

--- Game/Enemy/enemy.gd
+++ Game/Enemy/enemy.gd
@@ -1,2 +1,35 @@
 extends CharacterBody2D
 
+var current_Enemy_Coords
+
+func _ready():
+	# Obtener una referencia al TileMap
+	var tilemap = get_node("/root/World/TileMap")
+	# Obtener la matriz de adyacencia del TileMap
+	var adjacency_matrix = tilemap.get_matrix()
+	
+func get_Enemy_Coord():
+	# Obtener la posición actual del jugador
+	var enemy_position = global_position
+	#print(player_position)
+	# Convertir la posición del jugador a coordenadas de la matriz
+	var tilemap = get_node("/root/World/TileMap")
+	var tile_size = Vector2(16, 16)
+	var enemy_cell_x = int(enemy_position.x / tile_size.x)
+	var enemy_cell_y = int(enemy_position.y / tile_size.y)
+	
+	current_Enemy_Coords = Vector2(enemy_cell_x, enemy_cell_y)
+	#print("Posicion actual del enemigo en la matriz: ", current_Enemy_Coords)
+	return current_Enemy_Coords
+
+func get_Player_Coords():
+		# Obtener una referencia al TileMap
+	var player = get_node("/root/World/TileMap/Player")
+	# Obtener la matriz de adyacencia del TileMap
+	var playerCoords = player.get_Player_Coords()
+	var enemyCoords = get_Enemy_Coord()
+	
+	print("Player: ", playerCoords, " Enemy: ", enemyCoords)
+	
+func _physics_process(delta):
+	get_Enemy_Coord()
--- Game/player/player.gd
+++ Game/player/player.gd
@@ -3,6 +3,8 @@ extends CharacterBody2D
 @export var speed: int = 100
 @onready var animations = $AnimationPlayer
 
+var current_Player_Coords
+
 func handleInput():
 	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = moveDirection*speed
@@ -24,19 +26,24 @@ func _ready():
 	var tilemap = get_node("/root/World/TileMap")
 	# Obtener la matriz de adyacencia del TileMap
 	var adjacency_matrix = tilemap.get_matrix()
-
-func _physics_process(delta):
-	handleInput()
-	move_and_slide()
-	updateAnimation()
 	
+func get_Player_Coords():
 	# Obtener la posición actual del jugador
 	var player_position = global_position
-	print(player_position)
+	#print(player_position)
 	# Convertir la posición del jugador a coordenadas de la matriz
 	var tilemap = get_node("/root/World/TileMap")
 	var tile_size = Vector2(16, 16)
 	var player_cell_x = int(player_position.x / tile_size.x)
 	var player_cell_y = int(player_position.y / tile_size.y)
-	# Imprimir la posición del jugador en la matriz
-	print("Posición actual del jugador en la matriz: ", player_cell_x, ",", player_cell_y)
+	
+	current_Player_Coords = Vector2(player_cell_x, player_cell_y)
+	#print("Posicion actual del jugador en la matriz: ", current_Player_Coords)
+	return current_Player_Coords
+
+func _physics_process(delta):
+	handleInput()
+	move_and_slide()
+	updateAnimation()
+	get_Player_Coords()
+
