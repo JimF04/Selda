1600a559c20af7a25b1390d1686c5dc6702eb2c9
Kokigv <jorgutierrez@estudiantec.cr>
2024-04-28 17:18:01 -0600

    Camara 2D

--- ray_project/src/main.cpp
+++ ray_project/src/main.cpp
@@ -3,64 +3,68 @@
 #include "Wall.h"
 #include "Enemy.h"
 
-
-
 int main()
 {
-    
     Color darkGreen = Color{20, 160, 133, 255};
 
     const int screenWidth = 800;
     const int screenHeight = 600;
 
     Ball ball = Ball();
-
     InitWindow(screenWidth, screenHeight, "Control de bola con teclas WASD");
     SetTargetFPS(60);
 
     Wall pared = Wall();
     Enemy enemigo = Enemy();
 
+    // Caracteristicas de la camara
+    Camera2D camera = { 0 };
+    camera.target = (Vector2){ screenWidth / 2, screenHeight / 2 }; // Centro de la pantalla como su centro 
+    camera.offset = (Vector2){ screenWidth / 2, screenHeight / 2 };
+    camera.rotation = 0.0f; // Sin rotacion
+    camera.zoom = 1.0f; // Sin zoom
 
-
-  while (!WindowShouldClose())
-{
-    // Variables para almacenar el movimiento proyectado
-    int deltaX = 0;
-    int deltaY = 0;
-
-    
-
-    // Movimiento de la bola basado en las teclas WASD
-    if (IsKeyDown(KEY_W))
-        deltaY -= 5;
-    if (IsKeyDown(KEY_S))
-        deltaY += 5;
-    if (IsKeyDown(KEY_A))
-        deltaX -= 5;
-    if (IsKeyDown(KEY_D))
-        deltaX += 5;
-
-    // Calculamos la posición proyectada de la bola
-    Vector2 projectedPosition = {ball.GetPosition().x + deltaX, ball.GetPosition().y + deltaY};
-
-    // Comprobamos si la posición proyectada colisiona con la pared
-    Rectangle ballRect = {projectedPosition.x - ball.GetRadius(), projectedPosition.y - ball.GetRadius(), ball.GetRadius() * 2, ball.GetRadius() * 2};
-    if (!CheckCollisionRecs(ballRect, pared.GetCollisionRect()))
+    while (!WindowShouldClose())
     {
-        // Movemos la bola solo si no hay colisión
-        ball.Move(deltaX, deltaY);
+        // Update
+        int deltaX = 0;
+        int deltaY = 0;
+
+        // Handle input
+        if (IsKeyDown(KEY_W))
+            deltaY -= 5;
+        if (IsKeyDown(KEY_S))
+            deltaY += 5;
+        if (IsKeyDown(KEY_A))
+            deltaX -= 5;
+        if (IsKeyDown(KEY_D))
+            deltaX += 5;
+
+        Vector2 projectedPosition = { ball.GetPosition().x + deltaX, ball.GetPosition().y + deltaY };
+
+        Rectangle ballRect = { projectedPosition.x - ball.GetRadius(), projectedPosition.y - ball.GetRadius(), ball.GetRadius() * 2, ball.GetRadius() * 2 };
+        if (!CheckCollisionRecs(ballRect, pared.GetCollisionRect()))
+        {
+            ball.Move(deltaX, deltaY);
+        }
+
+        enemigo.FollowBreadcrumb(ball.GetPosition());
+
+        
+        camera.target = ball.GetPosition();// Siguiendo a la bola y su posicion
+
+        BeginMode2D(camera);  // Inicio del dibujo de la camara
+
+        
+        ClearBackground(darkGreen);
+        ball.Draw();
+        pared.Draw();
+        enemigo.Draw();
+
+        EndMode2D(); // Fin del dibujo de la camara
+
+        EndDrawing();
     }
-
-    enemigo.FollowBreadcrumb(ball.GetPosition());
-
-    BeginDrawing();
-    ClearBackground(darkGreen);
-    ball.Draw();
-    pared.Draw();
-    enemigo.Draw();
-    EndDrawing();
-}
 
     CloseWindow();
     return 0;
