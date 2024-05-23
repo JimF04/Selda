045038ba000d39e6730bcec04baeda75f0d29cba
gabrll05 <142859449+gabrll05@users.noreply.github.com>
2024-05-23 00:36:03 -0600

    fireballs

--- src/Enemy/Espectro.h
+++ src/Enemy/Espectro.h
@@ -27,19 +27,19 @@ public:
             // Determinamos la dirección de la bola de fuego según la dirección del jugador
             switch (currentDirection) {
                 case RIGHT:
-                    fireballVelocity.x = 1.5f; // Aumentamos la posición en 1 en el eje x
+                    fireballVelocity.x = 0.5; // Aumentamos la posición en 1 en el eje x
                     break;
                 case LEFT:
-                    fireballVelocity.x = -1.5f; // Reducimos la posición en 1 en el eje x
+                    fireballVelocity.x = -0.5f; // Reducimos la posición en 1 en el eje x
                     break;
                 case DOWN:
-                    fireballVelocity.y = 1.5f; // Aumentamos la posición en 1 en el eje y
+                    fireballVelocity.y = 0.5f; // Aumentamos la posición en 1 en el eje y
                     break;
                 case UP:
-                    fireballVelocity.y = -1.5f; // Reducimos la posición en 1 en el eje y
+                    fireballVelocity.y = -0.5f; // Reducimos la posición en 1 en el eje y
                     break;
                 case IDLE:
-                    fireballVelocity.x = 1.5f;
+                    fireballVelocity.x = 0.5f;
                     break;
                 default:
                     // No cambiamos la posición si la dirección no es válida
@@ -56,19 +56,19 @@ public:
 
 
     void UpdateFireballs() {
-
         // Actualizamos todas las bolas de fuego activas
         for (size_t i = 0; i < activeFireballs.size(); ++i) {
             activeFireballs[i].UpdatePosition(); // Actualizamos la posición de la bola de fuego
             activeFireballs[i].UpdateAnimation(); // Actualizamos la animación de la bola de fuego
 
-            float distanciaf = Vector2Distance(activeFireballs[i].GetPosition(), ball.GetPosition());
-
-//            if(distanciaf < ball.GetRadius() + 10) {
-//                ball.setPosition({1000,-1000});
-//                cout << "kjhilevuiwbevujibwujivbwikujbeviwbvicwbievbweiuvbkjdbshjkbejkcvbsiuebvjksbdhjke" << endl;
-//            }
-
+            // Calculamos la distancia entre la bola de fuego y la bola principal
+            float distanceToBall = Vector2Distance(activeFireballs[i].GetPosition(), ball.GetPosition());
+            cout << "Distancia entre bola de fuego y pelota: " << distanceToBall << endl;
+            // Comprobamos si hay colisión
+            if (distanceToBall < ball.GetRadius() + 150 ) {
+                ball.DecreaseLives(1);
+                cout << "Colisión detectada entre la bola de fuego y la pelota" << endl; // Imprimir un mensaje
+            }else
 
             // Verificamos la distancia recorrida por la bola de fuego
             if (Vector2Distance(activeFireballs[i].GetPosition(), position) > maxFireballDistance * 16) {
@@ -77,22 +77,10 @@ public:
                 // Decrementamos el índice para evitar omitir el siguiente elemento después de borrar uno
                 --i;
             }
-
-            // Colisiones con el jugador
-
-//            Vector2 firePos = activeFireballs[i].GetPosition();
-//            Vector2 playerPos = ball.GetPosition();
-
-
         }
         lastFireballTime += GetFrameTime();
-
-
-
-
-
-
     }
+
 
     void DrawFireballs() const {
         // Dibujamos todas las bolas de fuego activas
